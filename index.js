b83b98c6439428f9e2c60419b5bb7c9933186fe6
agxtn <augrp@aol.com>
2022-12-08 19:25:40 +0800

    Added uppercase first letters function

--- index.js
+++ index.js
@@ -33,15 +33,33 @@ convertLowerBtn.addEventListener("click", () => {
 })
 
 
-//If user text includes
+//Convert first letter to upper case 
+let convertFirstUpperBtn = document.getElementById("convertFirstUpper");
+
+convertFirstUpperBtn.addEventListener("click", () => {
+
+    userTextValue = userText.value;
+    splittedText = userTextValue.split(" ");
+    mappedText = splittedText.map(element => element[0].toUpperCase() + element.slice(1));
+    joinedText = mappedText.join(" ");
+    document.getElementById("resultText").textContent = `${joinedText}`;
+})
 
 
-let stringToFind = document.getElementById("doesContain")
 
-stringToFind.addEventListener("input", () => {
 
+/*If user text includes
+
+let stringToFind = document.getElementById("doesContain");
+let checkBtn = document.getElementById("checkText");
+
+checkBtn.addEventListener("click", () => {
+
+    stringToFind = stringToFind.value;
     let doesContainResult = (userText.value).includes(stringToFind);
 
     document.getElementById("containResult").textContent = `${doesContainResult}`;
 
-})
+}) */
+
+
